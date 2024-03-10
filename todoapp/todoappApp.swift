39eb3ba6389633bd724428b767e4962b633954e9
Thxmxs <thomas.c.arcos@gmail.com>
2024-03-10 15:44:00 -0300

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Thomas Cubillos arcos on 10-03-24.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
