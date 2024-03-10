27b3eff1cf56e107ad5001abb2d10d7b7d029e48
Thxmxs <thomas.c.arcos@gmail.com>
2024-03-10 18:07:45 -0300

    add, delete, favorite tasks

--- todoapp/ContentView.swift
+++ todoapp/ContentView.swift
@@ -8,17 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var description: String = ""
+    @StateObject var notesViewModel = NotesViewModel();
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Añade una tarea")
+                .underline()
+                .foregroundColor(.gray)
+                .padding(.horizontal,16)
+            TextEditor(text: $description)
+                .foregroundColor(.gray)
+                .frame(height: 100)
+                .overlay{
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.purple, lineWidth: 2)
+                }
+                .padding(.horizontal, 12)
+                .cornerRadius(3.0)
+            Button("Crear"){
+                notesViewModel.saveNote(descriptionAux: description)
+                description = ""
+            }
+            .buttonStyle(.bordered)
+            .tint(.purple)
+            Spacer()
+            
+            List{
+                ForEach($notesViewModel.notes, id: \.id){
+                    $note in
+                    HStack{
+                        if note.isFavorited{
+                            Text("🌟")
+                        }
+                        Text(note.description)
+                    }
+                    .swipeActions(edge: .trailing){
+                        Button{
+                            notesViewModel.updateFavoriteNote(note: $note)
+                        }
+                    label:{
+                        Label("Favorito",systemImage: "star.fill")
+                    }
+                    .tint(.yellow)
+                    }
+                    
+                    .swipeActions(edge: .leading){
+                        Button{
+                            notesViewModel.removeNote(withId: note.id)
+                        }
+                    label:{
+                        Label("Borrar",systemImage: "trash.fill")
+                    }
+                    .tint(.red)
+                    }
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Todo")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
--- todoapp/NoteViewModel.swift
+++ todoapp/NoteViewModel.swift
@@ -1,8 +1,41 @@
-//
-//  NoteViewModel.swift
-//  todoapp
-//
-//  Created by Thomas Cubillos arcos on 10-03-24.
-//
-
 import Foundation
+import SwiftUI
+
+class NotesViewModel: ObservableObject{
+    @Published var notes: [NoteModel] = []
+    
+    init() {
+        notes = getAllNotes()
+    }
+    
+    func saveNote(descriptionAux: String){
+        let newNote = NoteModel(description: descriptionAux)
+        notes.insert(newNote, at: 0)
+        encodeAndSaveAllNotes();
+    }
+    
+    private func encodeAndSaveAllNotes(){
+        if let encoded = try? JSONEncoder().encode(notes){
+            UserDefaults.standard.set(encoded, forKey: "notes")
+        }
+    }
+    
+    func getAllNotes() -> [NoteModel]{
+        if let notesData = UserDefaults.standard.object(forKey: "notes") as? Data{
+            if let notes = try? JSONDecoder().decode([NoteModel].self, from: notesData){
+                return notes
+            }
+        }
+        return []
+    }
+    
+    func removeNote(withId id: String){
+        notes.removeAll(where: {$0.id == id})
+        encodeAndSaveAllNotes()
+    }
+    
+    func updateFavoriteNote(note:  Binding<NoteModel>){
+        note.wrappedValue.isFavorited = !note.wrappedValue.isFavorited
+        encodeAndSaveAllNotes()
+    }
+}
