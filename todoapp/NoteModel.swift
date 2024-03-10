ac9a1c102f88d4032423b0b06f58c579d9f11c5c
Thxmxs <thomas.c.arcos@gmail.com>
2024-03-10 18:05:49 -0300

    add, delete, favorite tasks

---
+++ todoapp/NoteModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteModel.swift
+//  todoapp
+//
+//  Created by Thomas Cubillos arcos on 10-03-24.
+//
+
+import Foundation
---
+++ todoapp/NoteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteViewModel.swift
+//  todoapp
+//
+//  Created by Thomas Cubillos arcos on 10-03-24.
+//
+
+import Foundation
